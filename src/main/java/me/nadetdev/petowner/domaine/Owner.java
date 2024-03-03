56fe751665f05b04d6e427f23fa2e189f81d67f5
NadetDev <nadetdev@gmail.com>
2024-03-03 14:42:50 +0100

    Init commit

---
+++ src/main/java/me/nadetdev/petowner/domaine/Owner.java
@@ -0,0 +1,84 @@
+package me.nadetdev.petowner.domaine;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "owners", schema = "petowner")
+public class Owner implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "telephone")
+    private String telephone;
+
+    public Owner() {
+    }
+
+    public Owner(String firstName, String lastName, String address, String city, String telephone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
