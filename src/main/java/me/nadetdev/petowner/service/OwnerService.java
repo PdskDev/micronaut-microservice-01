8af133e2cf5b27075d5bf20cea388d57faac535e
NadetDev <nadetdev@gmail.com>
2024-03-03 16:27:26 +0100

    Add Owner service

---
+++ src/main/java/me/nadetdev/petowner/service/OwnerService.java
@@ -0,0 +1,15 @@
+package me.nadetdev.petowner.service;
+
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import me.nadetdev.petowner.domaine.Owner;
+
+import java.util.Optional;
+
+public interface OwnerService {
+    Owner save(Owner owner);
+    Page<Owner> findAll(Pageable pageable);
+    Optional<Owner> findOne(Long id);
+    void delete(Long id);
+
+}
---
+++ src/main/java/me/nadetdev/petowner/service/OwnerServiceImpl.java
@@ -0,0 +1,47 @@
+package me.nadetdev.petowner.service;
+
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import jakarta.inject.Singleton;
+import me.nadetdev.petowner.domaine.Owner;
+import me.nadetdev.petowner.repository.OwnerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Singleton
+@Transactional
+public class OwnerServiceImpl implements OwnerService {
+    private final Logger log = LoggerFactory.getLogger(OwnerServiceImpl.class);
+    private final OwnerRepository ownerRepository;
+
+    public OwnerServiceImpl(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        log.debug("Request to save Owner: {}", owner);
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public Page<Owner> findAll(Pageable pageable) {
+        log.debug("Request to get all owners");
+        return ownerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Owner> findOne(Long id) {
+        log.debug("Request to get Owner : {}", id);
+        return ownerRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete Owner : {}", id);
+        ownerRepository.deleteById(id);
+    }
+}
